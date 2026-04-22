f5c0b90c1593658b28faae5f7ab26939fdd55cbd
noon-abubakar <noon.abubakar@accenture.com>
2026-04-22 11:35:27 +0100

    Added functionality to redirect to sign in if user presses buy and isnt logged in, functionality to always show buy button and display a notifciation instead of redirecting to log in

--- src/main/java/com/reljicd/controller/ShoppingCartController.java
+++ src/main/java/com/reljicd/controller/ShoppingCartController.java
@@ -8,6 +8,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import java.util.HashMap;
+import java.util.Map;
+
 
 @Controller
 public class ShoppingCartController {
@@ -28,6 +33,25 @@ public class ShoppingCartController {
         modelAndView.addObject("products", shoppingCartService.getProductsInCart());
         modelAndView.addObject("total", shoppingCartService.getTotal().toString());
         return modelAndView;
+    }
+
+    @PostMapping("/shoppingCart/addProduct/{productId}")
+    public ResponseEntity<Map<String, Object>> addProductToCartAjax(@PathVariable("productId") Long productId) {
+        Map<String, Object> response = new HashMap<>();
+        productService.findById(productId).ifPresent(
+                product -> {
+                    shoppingCartService.addProduct(product);
+                    response.put("success", true);
+                    response.put("message", product.getName() + " added to cart");
+                });
+
+        if (!response.containsKey("success")) {
+            response.put("success", false);
+            response.put("message", "Product not found");
+        }
+        
+        return ResponseEntity.ok(response);
+    
     }
 
     @GetMapping("/shoppingCart/addProduct/{productId}")
